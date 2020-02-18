6877f52527f8bc591203bdd6d3926cd80935480e
SorinEni <60381859+SorinEni@users.noreply.github.com>
2020-02-18 07:46:37 +0100

    1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$name = "Sorin Eni" ;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About me</title>
+</head>
+<body>
+    
+    <h1><?php echo $name; ?></h1>
+    <ul>
+        <li>Narodil jsem se 4. února 2003, pocházim z Moldávie a jsem mužského pohlaví.</li>
+        <li>Na základní školu jsem chodil v Nymburce. A teď jsem na OSŠPo.</li>
+        <li>Moje koníčky jsou sledování filmů, seriálů a hraní her.</li>
+        <li>Do budoucna bych chtěl studovat a udělat si Ing. </li>
+        <li>Můj e-mail je esorin@seznam.cz</li>
+        <li>Tel. 776 685 572</li>
+    </ul>
+
+
+</body>
+</html>
