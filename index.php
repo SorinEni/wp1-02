f2222969f2135665595d44e3c063499c304d91c7
SorinEni <60381859+SorinEni@users.noreply.github.com>
2020-02-18 08:37:04 +0100

    proměny

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 $name = "Sorin Eni" ;
+$gender = "Muž" ;
+$school ="ZŠ Komenského Nymburk" ;
+$birthdate ="4.2.2003" ;
+$email ="esorin@seznam.cz" ;
+$tel ="776 685 572" ; 
+$hobbies ="filmy, seriály, hrani her"
 ?>
 
 <!DOCTYPE html>
@@ -14,12 +20,16 @@ $name = "Sorin Eni" ;
     
     <h1><?php echo $name; ?></h1>
     <ul>
-        <li>Narodil jsem se 4. února 2003, pocházim z Moldávie a jsem mužského pohlaví.</li>
-        <li>Na základní školu jsem chodil v Nymburce. A teď jsem na OSŠPo.</li>
-        <li>Moje koníčky jsou sledování filmů, seriálů a hraní her.</li>
-        <li>Do budoucna bych chtěl studovat a udělat si Ing. </li>
-        <li>Můj e-mail je esorin@seznam.cz</li>
-        <li>Tel. 776 685 572</li>
+        <li>Datum narozeni:</li>
+        <?php echo $birthdate; ?>
+        <li>Škola:</li>
+        <?php echo $school; ?>
+        <li>Koíčky:</li>
+        <?php echo $hobbies; ?>
+        <li>E-mail:</li>
+        <?php echo $email; ?>
+        <li>Tel:</li>
+        <?php echo $tel; ?>
     </ul>
 
 
